a280e291b0c62cc35e498cafb961287299c2f0d2
Rupoum <rupoummitra15278@gmail.com>
2025-02-14 22:45:54 +0530

    minor upodate

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code text-red-900">Hello</div>
+          <div>Hello how are you</div>
+          <div className="reviewbtn"></div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
